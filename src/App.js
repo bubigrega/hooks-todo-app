9e3b09451e6405a3088c0118a62c4ed59f422bee
Gregor Ahcin <bubi.grega@gmail.com>
2019-12-26 20:44:50 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  paper: {
+    height: "100vh"
+  }
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const classes = useStyles();
+
+  return <Paper className={classes.paper}>TODOS</Paper>;
 }
 
 export default App;
